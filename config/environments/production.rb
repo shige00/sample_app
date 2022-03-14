36ed0996f7b2dc3d27424ccc5a8f9335c8706580
yunununono <shige.ryu.ryu.3@i.softbank.jp>
2022-03-14 12:05:48 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier.new(harmony: true)
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier.new(harmony: true)
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
